f4e8a3c0f6a29f7de0418d9cf9243988c4456283
omerbilgetecan <64930222+omerbilgetecan@users.noreply.github.com>
2020-09-19 18:16:31 +0300

    Add files via upload

---
+++ bolumSistemi.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class bolumSistemi : MonoBehaviour
+{
+    public Button[] lvlButtons;
+    
+    private void Start()
+    {
+
+        int levelAt = PlayerPrefs.GetInt("levelAt", 1);
+
+        for (int i=0;i<lvlButtons.Length;i++)
+        {
+            if (i + 1 > levelAt)
+                lvlButtons[i].interactable = false;
+        }
+    }
+   
+
+    public void levelButtonsActive(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
